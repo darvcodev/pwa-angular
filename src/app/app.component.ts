c03d84d3a932a72db5afb5b64c1c7edfc3c4a4a0
darvcodev <rojasvidaldaniel@gmail.com>
2023-01-04 15:29:43 -0500

    feat: :fire: Actualizaciones automaticas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'pwa-angular';
+export class AppComponent implements OnInit {
+  title = 'PWA Angular';
+
+  constructor(private swUpdate: SwUpdate) {}
+
+  ngOnInit() {
+    this.updatePWA();
+  }
+
+  updatePWA = () => {
+    this.swUpdate.available.subscribe((value) => {
+      // if (confirm('New version available. Load New Version?')) {
+      //   window.location.reload();
+      // }
+      console.log('update:' + value);
+      window.location.reload();
+    });
+  };
 }
