9b5a93e1c60e4c26afc4c42544551c43675ff07b
darvcodev <rojasvidaldaniel@gmail.com>
2023-01-05 06:54:45 -0500

    fix: :art: Correccion archivo firebase

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -1,11 +1,8 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import firebase from "firebase/app";
+import "firebase/messaging";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://firebase.google.com/docs/web/learn-more#config-object
 const firebaseConfig = {
   apiKey: "AIzaSyAAEJL43L0Jns7BWL1meJ_jozUeLk14jNA",
   authDomain: "pwa-angular-2023.firebaseapp.com",
@@ -13,9 +10,11 @@ const firebaseConfig = {
   storageBucket: "pwa-angular-2023.appspot.com",
   messagingSenderId: "363981709882",
   appId: "1:363981709882:web:72c9b1d8e02d6fc7d2cb03",
-  measurementId: "G-D1GP7C08HS"
+  measurementId: "G-D1GP7C08HS",
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+firebase.initializeApp(firebaseConfig);
+
+// Initialize Firebase Cloud Messaging and get a reference to the service
+const messaging = firebase.messaging();
